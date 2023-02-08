4349696f10455b6aec18e3853cf7f2972def6366
mayank-kumar-arun <109716282+mayank-kumar-arun@users.noreply.github.com>
2023-02-08 19:27:34 +0530

    Created the ui for e-commerce site using styled components

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+const App = () => {
+  return <div>
+    {/* <Home></Home> */}
+    {/* <ProductList></ProductList> */}
+    {/* <Product></Product> */}
+    {/* <Register></Register> */}
+    {/* <Login></Login> */}
+    <Cart></Cart>
+  </div>;
+};
+
+export default App;
